a2230f74204ab7f3b458757b4a4347ed20329768
Majk336 <sojka.michal.96@gmail.com>
2018-10-30 15:11:28 +0100

    Added browser router

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
